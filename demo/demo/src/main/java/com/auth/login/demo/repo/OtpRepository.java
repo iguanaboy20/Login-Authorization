cc2013be11a976a726389e458194153b4e5bcbc9
iguanaboy20 <134500131+iguanaboy20@users.noreply.github.com>
2024-06-27 10:15:07 +0530

    Delete demo/demo directory

--- demo/demo/src/main/java/com/auth/login/demo/repo/OtpRepository.java
+++
@@ -1,9 +0,0 @@
-package com.auth.login.demo.repo;
-
-import com.auth.login.demo.model.Otp;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OtpRepository extends JpaRepository<Otp, Long> {
-
-    Otp findByUserEmailAndOtp(String email, String otp);
-}
--- demo/demo/src/main/java/com/auth/login/demo/repo/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.auth.login.demo.repo;
-
-import com.auth.login.demo.model.ProductItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<ProductItem,Long> {
-
-}
